fa41fe9ea71d1a334effda8f90ee94454ffa242a
Bhisma Bakhai <bhisma93@gmail.com>
2018-05-20 11:04:12 -0400

    insta

---
+++ js/insta.js
@@ -0,0 +1,13 @@
+var Ig_SEL = "[data-Ig]";
+var gallery_SEL = "[data-gallery]";
+
+var IgItems = document.querySelectorAll(Ig_SEL);
+var galleryTarget = document.querySelector(gallery_SEL);
+
+IgItems.forEach(function (pic) {
+  pic.addEventListener('mouseover', function (event) {
+    event.preventDefault();
+    galleryTarget.setAttribute('src', pic.getAttribute('href'));
+  })
+});
+
